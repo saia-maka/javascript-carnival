0313236de3a3b1d9ac18d6ad801c627bede53590
saia-maka <makasiosaia@gmail.com>
2022-05-12 09:22:15 +1200

    create onclick function

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,16 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+
+
+// Onclick function
+// create an array of elements to apply function to
+//loop through array and apply function to each element in array
+let unicorns = document.getElementsByTagName('img')
+console.log(unicorns)
+const clicked = () => {
+ console.log('Clicked!')
+}
+for (let i = 0; i < unicorns.length; i++) {
+  unicorns[i].onclick = clicked
+}
