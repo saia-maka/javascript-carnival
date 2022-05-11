664a930ddfcc148db92b66f5cd02af546c6f466f
saia-maka <makasiosaia@gmail.com>
2022-05-12 10:29:44 +1200

    create alert msg function and complete exercise

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -18,21 +18,39 @@ console.log('count is: ' + count)
 return count
 }
 
-// Onclick function
+// Onclick functions
+
+// create onclick function to return thank you message
+// function takes arugment representing unicorn number
+// create if count === 3 return alert message 'Unicorn number X says thank you!'
+let tymsg = (unicornNumber) => {
+if (count === 3) {
+  setTimeout(() => {
+    alert(`Unicorn number ${unicornNumber} says thank you!`)
+  }, 500)
+} else {
+  return
+}
+}
 
 // create variable representing currently clicked unicorn
+// find number of unicorn and place in variable
 // set attribute on targeted elm
 // use counter function to change attribute per click
+// call thank you msg function and enter unicorn number variable as argument
 let unicorns = document.getElementsByTagName('img')
 console.log(unicorns)
 const clicked = (e) => {
  let elm = e.target
-//  elm.setAttribute('src', counter(unicornImgs))
+ let unicornNum = elm.getAttribute('num')
   elm.setAttribute('src', `./images/unicorn-${counter()}.png`)
+  tymsg(unicornNum)
 }
 // create an array of elements to apply function to
+// set a number for each element in array
 //loop through array and apply function to each element in array
 for (let i = 0; i < unicorns.length; i++) {
+  unicorns[i].setAttribute('num', i + 1)
   unicorns[i].onclick = clicked
 }
 
