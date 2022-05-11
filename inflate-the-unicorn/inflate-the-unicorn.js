dddd8aa061dd784cdceede1630448189b3d33504
saia-maka <makasiosaia@gmail.com>
2022-05-12 10:04:40 +1200

    create image change functionality

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -4,8 +4,9 @@
 
 console.log("Inflate The Unicorn!")
 // setup 
-const unicornImgs = ['unicorn-0.png', 'unicorn-1.png', 'unicorn-2.png', 'unicorn-3.png']
+const unicornImgs = ['./images/unicorn-0.png', './images/unicorn-1.png', './images/unicorn-2.png', './images/unicorn-3.png']
 let count = 0;
+
 
 // create image counter function
 // increment count every time function is called
@@ -25,13 +26,17 @@ return arr[count]
 // Onclick function
 // create an array of elements to apply function to
 //loop through array and apply function to each element in array
+
+// create variable representing currently clicked unicorn
+// set attribute on targeted elm
+// use counter function to change attribute per click
 let unicorns = document.getElementsByTagName('img')
 console.log(unicorns)
-const clicked = () => {
- console.log('Clicked!')
- return counter()
+const clicked = (e) => {
+ let elm = e.target
+ elm.setAttribute('src', counter(unicornImgs))
 }
 for (let i = 0; i < unicorns.length; i++) {
-  unicorns[i].onclick = counter
+  unicorns[i].onclick = clicked
 }
 
