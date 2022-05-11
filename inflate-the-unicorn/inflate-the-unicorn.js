37c604e29a4e1dc6be35ef7fb2644fb8efe73e70
saia-maka <makasiosaia@gmail.com>
2022-05-12 09:52:18 +1200

    create counter function

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,6 +3,23 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+// setup 
+const unicornImgs = ['unicorn-0.png', 'unicorn-1.png', 'unicorn-2.png', 'unicorn-3.png']
+let count = 0;
+
+// create image counter function
+// increment count every time function is called
+// if count is greater than 3 reset counter to 0
+// return an array element at index number [count]
+let counter = (arr) => {
+count++
+if (count > 3) {
+  count = 0
+}
+console.log('count is: ' + count)
+return arr[count]
+}
+
 
 
 // Onclick function
@@ -12,7 +29,9 @@ let unicorns = document.getElementsByTagName('img')
 console.log(unicorns)
 const clicked = () => {
  console.log('Clicked!')
+ return counter()
 }
 for (let i = 0; i < unicorns.length; i++) {
-  unicorns[i].onclick = clicked
+  unicorns[i].onclick = counter
 }
+
