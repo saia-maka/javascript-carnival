80c410a1a206ac388adf4c4a85a72c46d4126119
saia-maka <makasiosaia@gmail.com>
2022-05-12 16:35:01 +1200

    create cells array and random number generator function

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -3,3 +3,26 @@
 // -    -   -   -   -  //
 
 console.log("Whack-a-Mole!")
+
+// - The ability to store or get references to the cells in the HTML table.
+// - A function to randomly select which cell should show the mole.
+// - A way to show a mole in the chosen cell.
+
+//setup
+
+// create an array of cells
+const cells = document.getElementsByTagName('td')
+// console.log(cells)
+
+// random number function
+// needs a min number variable & max number variable
+const minNum = 1
+const maxNum = cells.length
+// generate random number
+const getRandomNumber = (min, max) => {
+  console.log(Math.floor(Math.random() * max) + min)
+  // return Math.random()
+}
+
+
+
