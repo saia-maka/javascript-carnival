dbe91a27cde0748a68a2ae077ec5b22cc402e8a6
saia-maka <makasiosaia@gmail.com>
2022-05-12 20:55:31 +1200

    create js setup code

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -3,3 +3,28 @@
 // -    -   -   -   -  //
 
 console.log("Dress The Clown!")
+
+// Setup
+  //image arrays
+  const head = ['head0.png', 'head1.png', 'head2.png', 'head3.png', 'head4.png', 'head5.png']
+  const body = ['body0.png', 'body1.png', 'body2.png', 'body3.png', 'body4.png', 'body5.png']
+  const shoes = ['shoes0.png', 'shoes1.png', 'shoes2.png', 'shoes3.png', 'shoes4.png', 'shoes5.png']
+  // elements arrays
+  const elmArr = document.getElementsByTagName('img')
+  const elmIds = ['head', 'body', 'shoes']
+  // give elements id head, body, shoes
+  for (let i = 0; i < elmArr.length; i++) {
+    elmArr[i].setAttribute('id', elmIds[i])
+  }
+  // create counter variables
+  let headCount = 0;
+  let bodyCount = 0;
+  let shoesCount = 0;
+
+  
+
+
+// create function to change forward
+  // takes in array of images
+  // 
+const changeForward = () => {};
