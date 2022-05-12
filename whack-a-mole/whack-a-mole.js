e26d14741a5864d17d7e8412b50e39dc6ba39c9a
saia-maka <makasiosaia@gmail.com>
2022-05-12 17:13:26 +1200

    create mole shuffle function

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -12,21 +12,43 @@ console.log("Whack-a-Mole!")
 
 // create an array of cells
 const cells = document.getElementsByTagName('td')
-// console.log(cells)
+// console.log(cells[7])
+
+// add filepath to mole in a variable
+const mole = './mole.PNG'
+
+
 
 // random number function
 // needs a min number variable & max number variable
-const minNum = 1
-const maxNum = cells.length
+const minNum = 0
+const maxNum = cells.length 
 // generate random number
 const getRandomNumber = (min, max) => {
   // console.log(Math.floor(Math.random() * max) + min)
-  return Math.random(Math.floor(Math.random() * max) + min)
+  return Math.floor(Math.random() * max) + min
 }
 
-//create shuffle mole function access array and select random element with in using getRandomNumber
-
-
+// create shuffle mole function access array and select random element with in using getRandomNumber
+// add filepath to a variable and store in setup section
+// create funciton thats takes an array as arguments
+// select random element in arr and place in variable called molesHole
+// create a image element and store in a variable called moleImgElm
+// append and image element to molesHole
+// return
+const showMole = (arr) => {
+ const randomNum = getRandomNumber(minNum, maxNum)
+ console.log(randomNum)
+ const molesHole = cells[randomNum]
+//  console.log(molesHole)
+//  const moleImgElm = <img src="./mole.PNG"/>
+let moleImgElm = document.createElement('img')
+console.log(moleImgElm)
+moleImgElm.setAttribute('src', mole)
+moleImgElm.setAttribute('class', 'moleSize')
+ molesHole.appendChild(moleImgElm)
+}
+showMole()
 
 
 //create an onclick function to call shuffle mole function
